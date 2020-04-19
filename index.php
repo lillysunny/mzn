c604348d107d0eb0b3d33af7bd57ccc6a60eaf4e
lillysunny <63869626+lillysunny@users.noreply.github.com>
2020-04-19 07:20:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,17 @@
 	  <!-- Custom CSS -->
     <link rel="stylesheet" href="css/custom.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mega.nz Converter</title>
+  <title>Gmax Mega.nz Converter</title>
 </head>
 
 <body class="red lighter-3">
     <h3 class="white-text center">Mega.nz Converter</h3>
-    <h6 class="center white-text">Atenção: Esse script não funciona em localhost.</h6>
+    <h6 class="center white-text">UNLIMITED DOWNLAOD.</h6>
     <div class="container center white container" style="margin-top:2%;">
         <div class="row center" style="padding:5%;">
           <div class="center col s12" style="margin-bottom:20px;">
             <input id="mega_url" type="text">
-            <label for="mega_url">Link para Converter</label>
+            <label for="mega_url">Link Converter</label>
             <br>
             <a style="margin-top:10px;" id="btn_convert" onclick="convertLink()" class="waves-effect waves-light btn green">Converter</a>
           </div>
@@ -35,7 +35,7 @@
         </div>
     </div>
 
-    <h6 class="center white-text">Made With &hearts; by GMAX</h6>
+    <h6 class="center white-text">Made With &hearts; by GMAX@2020</h6>
 
      <!-- Jquery -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
